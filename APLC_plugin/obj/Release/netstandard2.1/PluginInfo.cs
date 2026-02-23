171663087fc6b741766fa86b95aea65e7318d08a
IThisGuyHere <cnijdl1@gmail.com>
2026-02-22 21:55:04 -0700

    removed bin/ and obj/

--- APLC_plugin/obj/Release/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace APLC
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "APLC";
-        public const string PLUGIN_NAME = "APLC";
-        public const string PLUGIN_VERSION = "0.7.9";
-    }
-}
