024ed580cb96ea661d5ab297157768b9b3d2e31e
IThisGuyHere <cnijdl1@gmail.com>
2026-04-29 11:49:30 -0700

    scrap that spawns in custom interiors is now in logic

---
+++ APLC_plugin/LLLCompat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx.Bootstrap;
+using Steamworks.Ugc;
+
+namespace APLC
+{
+    internal class LLLCompat
+    {
+        public static bool IsLethalLevelLoaderInstalled => Chainloader.PluginInfos.ContainsKey(LethalLevelLoader.Plugin.ModGUID);
+
+        public static double GetDynamicRarityForAllDungeons(Item item, SelectableLevel moon, int totalInteriorRarity)
+        {
+            var extItem = LethalLevelLoader.PatchedContent.ExtendedItems.Find(extItem => extItem.Item.Equals(item));
+            if (extItem == null) return 0;
+            double rarity = 0;
+            foreach (var interior in LethalLevelLoader.DungeonManager.GetValidExtendedDungeonFlows(LethalLevelLoader.LevelManager.GetExtendedLevel(moon), false))
+            {
+                int scrapWeight = extItem.DungeonMatchingProperties.GetDynamicRarity(interior.extendedDungeonFlow);
+                if (scrapWeight > 0) rarity += scrapWeight * ((double)interior.rarity / totalInteriorRarity);
+            }
+            return rarity;
+        }
+    }
+}
